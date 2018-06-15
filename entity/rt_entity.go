fc400d14eadf33ebcd1344ea33fb3f4fc4f1c3cb
Caroline1997 <850922107@qq.com>
2018-06-16 01:53:00 +0800

    add rt entity

---
+++ entity/rt_entity.go
@@ -0,0 +1,51 @@
+/********************************
+restaurant的数据层，保存基本信息
+*********************************/
+
+package rt 
+
+import (
+)
+
+// Dish 单个菜品的信息
+type Dish struct {
+	//菜品名称 不会有相同的
+	DishName string `json:"dishname"`
+	//菜品描述
+	DishDes string `json:"dishdes"`
+	//菜品价格
+	DishPrice float32 `json:"dishprice"`
+	
+	//size? 
+	
+	//菜品照片 url
+	DishPicture string `json:"dishpicture_url"`
+	//菜品已售出数量
+	DishSales int `json:"dishsales"`
+	//category? 
+}
+
+//每个餐厅的信息
+type Rt struct {
+	RtName string `json:"rtname"`
+	RtLocation string `json:"rtlocation"`
+	RtDes string `json:"rtdes"`
+	RtLogo string `json:"rtlogo_url"`
+    RtDishes []Dish `json:"rtdishes"`
+}
+
+// orderitem 每个订单菜品的信息
+type Orderitem struct {
+	ItemName string `json:"itemname"`
+	//ItemSize?
+	ItemAmount int `json:"itemamount"`
+}
+
+// order 每个订单的信息
+type Order struct {
+	OrderDesk int `json:"orderdesk"`
+	//order_number?
+	OrderPrice
+	OrderIspayed bool `json:"orderispayed"`
+	OrderItems []Orderitem `json:"orderitems"`
+}
