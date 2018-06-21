893c288ce272b2e76ce9ab500343ad95fad46dec
Caroline1997 <850922107@qq.com>
2018-06-21 16:26:55 +0800

    complete entity

--- entity/rt_entity.go
+++ entity/rt_entity.go
@@ -15,37 +15,44 @@ type Dish struct {
 	DishDes string `json:"dishdes"`
 	//菜品价格
 	DishPrice float32 `json:"dishprice"`
-	
 	//size? 
-	
 	//菜品照片 url
 	DishPicture string `json:"dishpicture_url"`
 	//菜品已售出数量
 	DishSales int `json:"dishsales"`
-	//category? 
+	//菜品所属种类
+	DishCategory string `json:"category"` 
 }
-
-//每个餐厅的信息
+// Category 类别
+type Category struct {
+	//类别名称
+	CategoryName string `json:"cgname"`
+	//类别包含的菜品
+	DishesContained []Dish `json:"dishes"`
+}
+// Rt 每个餐厅的信息
 type Rt struct {
 	RtName string `json:"rtname"`
 	RtLocation string `json:"rtlocation"`
 	RtDes string `json:"rtdes"`
 	RtLogo string `json:"rtlogo_url"`
-    RtDishes []Dish `json:"rtdishes"`
+	//RtDishes []Dish `json:"rtdishes"`
+	RtCategories []Category `json:"rtcatogories"`
 }
 
-// orderitem 每个订单菜品的信息
+// Orderitem 每个订单菜品的信息
 type Orderitem struct {
 	ItemName string `json:"itemname"`
 	//ItemSize?
 	ItemAmount int `json:"itemamount"`
 }
 
-// order 每个订单的信息
+// Order 每个订单的信息
 type Order struct {
+	OrderID int `json:"orderid"`
 	OrderDesk int `json:"orderdesk"`
 	//order_number?
-	OrderPrice
+	OrderPrice float64 `json:"orderprice"`
 	OrderIspayed bool `json:"orderispayed"`
 	OrderItems []Orderitem `json:"orderitems"`
 }
